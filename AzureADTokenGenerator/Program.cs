872071787485da89e4dbc6800ac50c19d92a7b67
Kunal Chandratre <kunalchandratre@gmail.com>
2019-04-29 08:47:21 -0700

    unwanted secrets removed

--- AzureADTokenGenerator/Program.cs
+++ AzureADTokenGenerator/Program.cs
@@ -11,9 +11,7 @@ namespace AzureADTokenGenerator
     {
         static void Main(string[] args)
         {
-            var clientCredential = new ClientCredential("bef5d8be-6567-424f-95d4-af990a3a8d53", "S0R/EI6LtPqRI3VtdtV/3ej1ZWUt49+pidVf/Bd4W1U=");
-            //var clientCredential = new ClientCredential("Your Azure AD Application ID", "Azure AD Application secret key");
-            var authenticationContext = new AuthenticationContext("https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47");
+            //var clientCredential = new ClientCredential("Your Azure AD Application ID", "Azure AD Application secret key");            
             //var authenticationContext = new AuthenticationContext("https://login.microsoftonline.com/YourAzureADTenantID");
             var result = authenticationContext.AcquireTokenAsync("https://management.azure.com/", clientCredential).Result;
 
